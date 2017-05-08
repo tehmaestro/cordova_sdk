4375bc8065002a659c16d5f32dae3bed10f10240
Obaied <abdullah.obaied@gmail.com>
2017-05-08 16:37:32 +0200

    Modify the way the location of the output JAR file in test scripts to accomodate for the new example_ci project

--- ext/Android/build.sh
+++ ext/Android/build.sh
@@ -5,13 +5,15 @@
 #   - makeJar
 # - Copy the JAR file to the root dir
 
-
-BUILD_DIR=./sdk/Adjust
 OUT_DIR=./adjust/build/outputs
 
-# cd to the called directory to be able to run the script from anywhere
-cd $(dirname $0) 
+# Get the script directory and Traverse up to get to the root SDK directory
+SDK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SDK_DIR="$(dirname "$SDK_DIR")"
+SDK_DIR="$(dirname "$SDK_DIR")" 
 
-cd $BUILD_DIR
+# cd to the called directory to be able to run the script from anywhere
+cd $SDK_DIR/ext/android/sdk/Adjust
+
 ./gradlew clean clearJar makeJar
-mv -v ${OUT_DIR}/*.jar ../../../../src/Android/adjust-android.jar
+mv -v ${OUT_DIR}/*.jar $SDK_DIR/ext/Android/adjust-android.jar
