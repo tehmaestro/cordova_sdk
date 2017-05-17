1f04245da44678493172c06db75c475865f11cac
Obaied <abdullah.obaied@gmail.com>
2017-05-17 15:48:12 +0200

    Add adjust testing module as a standalone folder

---
+++ cordova-plugin-adjust-testing/www/adjust_testing.js
@@ -0,0 +1,18 @@
+function callCordovaCallback(action, callback) {
+    var args = Array.prototype.slice.call(arguments, 2);
+
+    cordova.exec(callback,
+        function errorHandler(err) { },
+        'AdjustTesting',
+        action,
+        args
+    );
+}
+
+var AdjustTesting = {
+    initTestSession: function(baseUrl, callback) {
+        callCordovaCallback('initTestSession', callback, baseUrl);
+    }
+};
+
+module.exports = AdjustTesting;
