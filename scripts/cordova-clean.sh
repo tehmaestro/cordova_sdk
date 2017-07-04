a905c0ab54f6e22036dc2d517d0202e424fd0503
Obaied <abdullah.obaied@gmail.com>
2017-07-04 17:58:09 +0200

    Modify scripts

--- scripts/cordova-clean.sh
+++ scripts/cordova-clean.sh
@@ -10,6 +10,7 @@ SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$(dirname "$SCRIPTS_DIR")"
 EXAMPLE_DIR=example_ci
 SDK_NAME=com.adjust.sdk
+TESTING_PLUGIN_NAME=com.adjust.sdktesting
 
 RED='\033[0;31m' 	# Red color
 GREEN='\033[0;32m' 	# Green color
@@ -18,6 +19,7 @@ NC='\033[0m' 		# No Color
 echo -e "${GREEN}>>> Removing cordova plugins ${NC}"
 cd ${ROOT_DIR}/${EXAMPLE_DIR}
 cordova plugin rm ${SDK_NAME}
+cordova plugin rm ${TESTING_PLUGIN_NAME}
 cordova plugin rm cordova-plugin-console
 cordova plugin rm cordova-plugin-customurlscheme
 cordova plugin rm cordova-plugin-dialogs
@@ -25,7 +27,8 @@ cordova plugin rm cordova-plugin-whitelist
 cordova plugin rm cordova-plugin-device
 cordova plugin rm cordova-universal-links-plugin
 
-rm -rv plugins/com.adjust.sdk
+rm -rv plugins/${SDK_NAME}
+rm -rv plugins/${TESTING_PLUGIN_NAME}
 
 cordova platform remove ios
 cordova platform remove android
